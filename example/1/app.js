49381d9cc0f9b832da8ca316725a1e4c67208103
Lucas Myers <lucas@unthunk.com>
2015-01-31 19:56:55 -0800

    example 1

---
+++ example/1/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* global angular */
+
+// some handy variables
+var app = {
+  name: 'beerApp'
+};
+
+// make it Angular
+var beerApp = angular.module(app.name, []);
+
+
+// We aren't really doing anything appy yet!
